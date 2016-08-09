b53be699efb54f25534060d88d60869d5d105662
Denis Immoos <denisimmoos@gmail.com>
2016-08-09 15:37:13 +0200

    check_oracle_archive_log.pl

---
+++ check_oracle_archive_log.pl
@@ -0,0 +1,182 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: check_oracle_archive_log.pl
+#
+#        USAGE: ./check_oracle_archive_log.pl  
+#
+#  DESCRIPTION: Checks multiple oracle tablespaces
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Denis Immoos (<denisimmoos@gmail.com>)
+#    AUTHORREF: Senior Linux System Administrator (LPIC3)
+#      VERSION: 1.0
+#      CREATED: 01/20/2016 09:26:28 AM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+#use lib '/usr/lib64/nagios/plugins/check_oracle/lib';
+use lib '/home/monitor/check_oracle/lib';
+
+#===============================================================================
+# MODULES
+#===============================================================================
+
+use Module::Load;
+use Data::Dumper;
+
+#===============================================================================
+# OPTIONS
+#===============================================================================
+
+my %Options = ();
+$Options{'print-options'} = "yes";
+
+#===============================================================================
+# SYGNALS - to syslog
+#===============================================================================
+
+# You can get all SIGNALS by:
+# perl -e 'foreach (keys %SIG) { print "$_\n" }'
+# $SIG{'INT'} = 'DEFAULT';
+# $SIG{'INT'} = 'IGNORE';
+
+sub INT_handler {
+    my($signal) = @_;
+    chomp $signal;
+    use Sys::Syslog;
+    my $msg = "INT: int($signal)\n";
+    print $msg;
+    syslog('info',$msg);
+    exit(0);
+}
+$SIG{INT} = 'INT_handler';
+
+sub DIE_handler {
+    my($signal) = @_;
+    chomp $signal;
+    use Sys::Syslog;
+    my $msg = "DIE: die($signal)\n";
+    print $msg;
+    syslog('info',$msg);
+}
+$SIG{__DIE__} = 'DIE_handler';
+
+sub WARN_handler {
+    my($signal) = @_;
+    chomp $signal;
+    use Sys::Syslog;
+    my $msg = "WARN: warn($signal)\n";
+    print $msg;
+    syslog('info',$msg);
+}
+$SIG{__WARN__} = 'WARN_handler';
+
+ 
+#===============================================================================
+# Getopt::Long;
+#===============================================================================
+
+
+
+use Getopt::Long;
+Getopt::Long::Configure ("bundling");
+GetOptions(\%Options,
+    'v',    'verbose',
+    'h',    'help',
+    'H:s',  'hostname:s',
+    'A:s',  'authfile:s',
+    'S:s',  'sid:s',
+    'C:s',  'critical:s',
+            'username:s',      #
+            'password:s',      #
+);
+
+#===============================================================================
+# PARSE OPTIONS
+#===============================================================================
+
+my $ParseOptions = 'OracleArchiveLog::ParseOptions';
+load $ParseOptions;
+$ParseOptions = $ParseOptions->new();
+%Options = $ParseOptions->parse(\%Options);
+
+
+#===============================================================================
+# SQL
+#===============================================================================
+
+my $SQL = 'OracleArchiveLog::SQL';
+load $SQL;
+$SQL = $SQL->new();
+%Options = $SQL->sql(\%Options);
+
+#===============================================================================
+# MAIN
+#===============================================================================
+
+#===============================================================================
+# Nagios
+#===============================================================================
+
+my %NagiosStatus = (
+    OK       => 0,
+    WARNING  => 1,
+    CRITICAL => 2,
+    UNKNOWN  => 3,
+
+    0       => 'OK',
+    1       => 'WARNING',
+    2       => 'CRITICAL',
+    3       => 'UNKNOWN',
+);
+
+if ( $Options{'LOG_MODE'}{'LOG_MODE'} eq 'ARCHIVELOG' ) {
+    # defaults
+	$Options{'nagios-msg'} = $NagiosStatus{0};
+	$Options{'nagios-status'} = $NagiosStatus{'OK'};
+} else {
+	# NOARCHIVELOG
+	$Options{'nagios-msg'} = $NagiosStatus{2};
+	$Options{'nagios-status'} = $NagiosStatus{'CRITICAL'};
+}
+
+
+print "$Options{'nagios-msg'} - $Options{'LOG_MODE'}{'LOG_MODE'}" . "\n";
+exit($Options{'nagios-status'});
+
+
+__END__
+
+=head1 NAME
+
+check_oracle_archive_log.pl 
+
+=head1 SYNOPSIS
+
+./check_oracle_archive_log.pl 
+
+=head1 DESCRIPTION
+
+This description does not exist yet, it
+was made for the sole purpose of demonstration.
+
+=head1 LICENSE
+
+This is released under the GPL3.
+
+=head1 AUTHOR
+
+Denis Immoos - <denisimmoos@gmail.com>, Senior Linux System Administrator (LPIC3)
+
+
+
+
+
